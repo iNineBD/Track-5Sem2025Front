9f9a85529fe2647ee68fbb2a4e62a4f610201867
William Antoniazzi <william.antoniazzi@outlook.com>
2025-03-18 22:29:04 -0300

    Added ESLint+Prettier  module at the project

--- components/the-header/index.vue
+++ components/the-header/index.vue
@@ -53,7 +53,7 @@ const isOpen = ref(false)
       />
     </div>
 
-    <USlideover class="max-w-36 ml-auto" v-model="isOpen">
+    <USlideover v-model="isOpen" class="max-w-36 ml-auto">
       <div class="p-4 flex flex-col h-full">
         <UButton
           icon="i-heroicons-x-mark-20-solid"
@@ -72,8 +72,8 @@ const isOpen = ref(false)
                 :color="$route.path === item.to ? 'primary' : 'gray'"
                 variant="ghost"
                 class="w-full font-bold"
-                @click="isOpen = false"
                 size="lg"
+                @click="isOpen = false"
               >
                 {{ item.label }}
               </UButton>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,15 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui', '@nuxt/image'],
+  modules: ['@nuxt/ui', '@nuxt/image', '@nuxt/eslint' ],
   colorMode: {
     preference: 'light'
+  },
+  eslint: {
+    checker: true,
+    config: {
+      stylistic: true,
+    },
   },
   vite: {
     server: {

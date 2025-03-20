077eae3a614cd9c9d5feb2a288f141474375a8e6
alimkhodr <ali.m.khodr@gmail.com>
2025-03-20 19:55:08 -0300

    feat: implement project fetching logic and configure Axios for API requests

---
+++ composables/useProjects.ts
@@ -0,0 +1,22 @@
+import { useNuxtApp, useRuntimeConfig } from '#app'
+
+export function useProjects() {
+  const { $api } = useNuxtApp();
+  const config = useRuntimeConfig();
+
+  const { data, pending, error } = useAsyncData('fetchProjects', async () => {
+    try {
+      const response = await $api.get(`${config.public.apiServer}/projects/data`);
+      return { projects: response.data.success };
+    } catch (error) {
+      console.error('Erro ao buscar projetos:', error);
+      return { projects: [], error: error instanceof Error ? error.message : 'Erro desconhecido' };
+    }
+  });
+
+  return {
+    data: computed(() => data.value?.projects || []),
+    pending,
+    error: computed(() => data.value?.error || null)
+  };
+}
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,11 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: ['@nuxt/ui', '@nuxt/image'],
+  runtimeConfig: {
+    public: {
+      apiServer: process.env.API_SERVER
+    }
+  },
   colorMode: {
     preference: 'light'
   },

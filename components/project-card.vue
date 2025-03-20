a5362e0610df16c3013decb6c391342f43327563
alimkhodr <ali.m.khodr@gmail.com>
2025-03-20 19:55:56 -0300

    feat: refactor project card component to use project object and enhance project details display

--- components/project-card.vue
+++ components/project-card.vue
@@ -1,65 +1,65 @@
 <script setup lang="ts">
-import type { Member } from '~/general-types';
+import type { Project } from '~/types';
+import { formatDate, differenceBetweenDays, daysAgo } from '~/utils';
 
 const props = defineProps<{
-  title: string;
-  description: string;
-  createdAt: string;
-  modifiedAt: string;
-  totalCards: number;
-  picture: string;
-  members: Member[];
-  productManager: Member;
+  project: Project;
 }>();
 </script>
 
 <template>
   <UCard
-    class="w-full hover:scale-[1.02] hover:border border-gray-200 dark:border-gray-700 duration-300 ease-in cursor-pointer">
+    class="w-full hover:scale-[1.02] hover:border border-gray-200 dark:border-gray-700 duration-300 ease-in cursor-pointer"
+  >
     <template #header>
       <div class="flex flex-row items-center space-x-4">
-        <NuxtImg :src="props.picture" alt="Project Image" class="h-16 rounded-md" />
+        <UAvatar 
+          :alt="project.name"
+          size="3xl"
+        />
         <div>
-          <UText tag="h2" size="large" color="primary" weight="semi-bold" class="mb-0 line-clamp-1 text-ellipsis">
-            {{ props.title }}
+          <UText
+            tag="h2"
+            size="extra-large"
+            color="primary"
+            weight="semi-bold"
+            class="mb-0 line-clamp-1 text-ellipsis"
+          >
+            {{ project.name }}
           </UText>
-          <UText size="small" weight="normal" class="mt-0 line-clamp-1 text-ellipsis">
-            {{ props.description }}
+          <UText
+            size="small"
+            weight="normal"
+            class="line-clamp-2"
+          >
+            {{ project.description }}
           </UText>
         </div>
       </div>
     </template>
-    <div class="flex flex-col gap-3">
-      <div class="flex justify-between items-center">
-        <UText size="medium" weight="bold">
-          Product Manager
-        </UText>
-        <UBadge icon="heroicons:rectangle-stack-20-solid" size="sm" color="white" variant="solid"
-          :label="props.productManager.alt" :trailing="false">
-          <template #leading>
-            <UAvatar :src="props.productManager.src" size="2xs" />
-          </template>
-        </UBadge>
-      </div>
-      <div class="flex justify-between items-center">
-        <UText size="medium" weight="bold">
-          Membros
-        </UText>
-        <UAvatarGroup size="xs" :max="2">
-          <UAvatar v-for="member in props.members" :key="member.id_member" :src="member.src" :alt="member.alt" />
-        </UAvatarGroup>
-      </div>
-    </div>
-    <div class="flex justify-between items-center">
-      <UText size="medium" weight="bold">
-        Cards
+    <div class="flex flex-col space-y-4 items-start">
+      <UBadge :color="project.finish_date ? 'green' : 'yellow'" :label="project.finish_date ? 'Finalizado' : 'Em andamento'" />
+      <UText 
+        size="medium"
+        weight="normal"
+        class="mt-2"
+      >
+        Duração: {{ differenceBetweenDays(props.project.created_date, props.project.finish_date) }} dias
       </UText>
-      <UBadge icon="heroicons:rectangle-stack-20-solid" size="sm" color="white" variant="solid"
-        :label="props.totalCards" :trailing="false" />
+      <UText 
+        size="medium"
+        weight="normal"
+        class="mt-2"
+      >
+        Última atualização há {{ daysAgo(props.project.modified_date) }} dias
+      </UText>
     </div>
     <template #footer>
-      <UText size="small" weight="normal">
-        {{ new Date(props.createdAt).toLocaleDateString('pt-BR', { day: 'numeric', month: 'long', year: 'numeric' }) }}
+      <UText 
+        size="small"
+        weight="normal"
+      >
+        {{ formatDate(project.created_date) }}
       </UText>
     </template>
   </UCard>
--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,48 @@
 <script setup lang="ts">
-import projects from '~/assets/data/projects.json';
+import { useProjects } from '~/composables/useProjects';
 
 useHead({
   title: 'Home - Track'
 });
+
+const { data: projects, pending: loadingProjects, error } = useProjects();
+const toast = useToast()
+
+if (error.value) {
+  toast.add({
+    icon: 'i-heroicons-x-circle',
+    color: 'red',
+    title: 'Error',
+    description: 'Falha ao carregar projetos. Tente novamente mais tarde.',
+    timeout: 0,
+  });
+}
 </script>
 
 <template>
   <div>
-    <UText tag="h1" size="extra-large" weight="bold">
+    <UText 
+      tag="h1"
+      size="title"
+      weight="bold"
+    >
       Meus Projetos
     </UText>
-
-    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
-      <ProjectCard
-        v-for="project in projects.projects"
-        :key="project.id_project"
-        :title="project.name"
-        :description="project.description"
-        :created-at="project.created_date"
-        :modified-at="project.modified_date" 
-        :total-cards="project.total_cards"
-        :picture="project.picture"
-        :members="project.members"
-        :product-manager="project.product_manager"
-      />
+    <div>
+      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+        <USkeleton
+          v-if="loadingProjects || error" v-for="n in 4"
+          class="w-full h-80"
+          :ui="{ rounded: 'rounded-lg' }"
+        />
+        <ProjectCard
+          v-else
+          v-for="project in projects"
+          :key="project.id"
+          :project="project"
+          @click="navigateTo('/projects/' + project.id)"
+        />
+      </div>
     </div>
   </div>
 </template>

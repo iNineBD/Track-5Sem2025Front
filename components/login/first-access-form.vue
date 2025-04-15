4182104ffdcf6340b96d718c371e979ac705118a
AlitaAmancio <amancioalita@gmail.com>
2025-04-14 22:52:06 -0300

    feat: creating login components

---
+++ components/login/first-access-form.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { reactive } from "vue";
+import type { FormError } from "#ui/types";
+
+const state = reactive({
+  email: "",
+});
+
+const validate = (state: any): FormError[] => {
+  const errors = [];
+  if (!state.email)
+    errors.push({ path: "email", message: "Email obrigatório." });
+  return errors;
+};
+
+const sendToken = async () => {
+  console.log("Token enviado");
+  // Your sendToken logic here
+};
+</script>
+
+<template>
+  <div>
+    <UForm
+      :validate="validate"
+      :state="state"
+      class="space-y-4"
+      @submit="sendToken"
+    >
+      <UFormGroup label="Email" name="email">
+        <UInput v-model="state.email" type="email" placeholder="Email" />
+      </UFormGroup>
+    </UForm>
+  </div>
+</template>
--- layouts/login.vue
+++ layouts/login.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <p>Layout do login (sem header)</p>
     <slot />
   </div>
 </template>

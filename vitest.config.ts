a2f38bc96ca546d83d35de8fb9c9514f3a6e109b
William Antoniazzi <william.antoniazzi@outlook.com>
2025-04-20 16:33:24 -0300

    fix/TRK-66: Corrections in Sonar coverage and workflow actions

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,7 @@ import { defineVitestConfig } from "@nuxt/test-utils/config";
 export default defineVitestConfig({
   test: {
     environment: "nuxt",
-    environmentOptions: {
+    workspace: {
       nuxt: {
         mock: {
           intersectionObserver: true,
@@ -12,8 +12,12 @@ export default defineVitestConfig({
       },
     },
     coverage: {
-      reporter: ["text", "lcov"],
+      provider: "v8", // Use o provider v8 para cobertura de código
+      reporter: ["text", "lcov", "json"], // Escolha os formatos de saída desejados
+      include: ["src/**/*.{js,ts,vue}"], // Defina quais arquivos devem ser analisados para cobertura
       reportsDirectory: "./coverage",
     },
   },
 });
+
+// environmentOptions: {},

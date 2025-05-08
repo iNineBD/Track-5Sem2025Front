e84cb43ec8de9a82af9a15a0eb5a2279c9668dde
alimkhodr <ali.m.khodr@gmail.com>
2025-05-08 20:17:30 -0300

    TRK-94 feat: add users page with authentication middleware and metadata

---
+++ pages/users.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+definePageMeta({
+  middleware: "auth",
+  name: "users",
+  path: "/usuarios",
+});
+
+useHead({
+  title: "Usuários - Track",
+});
+</script>
+
+<template><UText>Users</UText></template>

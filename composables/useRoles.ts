2a784579371a5b9cf5ef2b477053b853e8298e3e
alimkhodr <ali.m.khodr@gmail.com>
2025-05-13 20:44:46 -0300

    TRK-94 feat: add useRoles composable for fetching user roles from the API

---
+++ composables/useRoles.ts
@@ -0,0 +1,31 @@
+import { AxiosError } from "axios";
+
+export async function useRoles() {
+  const { $api } = useNuxtApp();
+  const config = useRuntimeConfig();
+
+  try {
+    const response = await $api.get(
+      `${config.public.apiServer}/api/usermanagement/data/roles`,
+    );
+
+    return response.data;
+  } catch (error) {
+    let errorMessage;
+
+    if (error instanceof AxiosError && error.response) {
+      if (error.response.data && error.response.data.error) {
+        errorMessage = error.response.data.error;
+      }
+    } else if (error instanceof Error) {
+      errorMessage = error.message;
+    }
+
+    console.error("Erro ao buscar roles:", errorMessage);
+
+    return {
+      success: false,
+      message: errorMessage,
+    };
+  }
+}

c73b7ec5832d153f7ada0d8bada4b62ba9690075
alimkhodr <ali.m.khodr@gmail.com>
2025-05-13 20:45:03 -0300

    TRK-94 feat: add useUpdateRole composable for updating user roles via API

---
+++ composables/useUpdateRoles.ts
@@ -0,0 +1,41 @@
+import { AxiosError } from "axios";
+
+export async function useUpdateRole({
+  id_role,
+  id_user,
+}: {
+  id_role: number;
+  id_user: number;
+}) {
+  // Debug log to verify correct parameter values
+  console.log("Updating user role", { id_role, id_user });
+
+  const { $api } = useNuxtApp();
+  const config = useRuntimeConfig();
+
+  try {
+    const response = await $api.put(
+      `${config.public.apiServer}/api/usermanagement/update`,
+      { id_role, id_user },
+    );
+
+    return response.data;
+  } catch (error) {
+    let errorMessage;
+
+    if (error instanceof AxiosError && error.response) {
+      if (error.response.data && error.response.data.error) {
+        errorMessage = error.response.data.error;
+      }
+    } else if (error instanceof Error) {
+      errorMessage = error.message;
+    }
+
+    console.error("Erro ao atualizar role:", errorMessage);
+
+    return {
+      success: false,
+      message: errorMessage,
+    };
+  }
+}

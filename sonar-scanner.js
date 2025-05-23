0cb3878ffe5c9824bd4270c3b4b583ab9cd655fa
William Antoniazzi <william.antoniazzi@outlook.com>
2025-05-22 22:54:26 -0300

    TRK-106 - fix: Created Sonarqube Community and sent actions to two environments via .yml

--- sonar-scanner.js
+++ sonar-scanner.js
@@ -1,17 +1,17 @@
 // sonar-scanner.js
-const scanner = require('sonarqube-scanner');
+const scanner = require("sonarqube-scanner");
 
 scanner(
   {
-    serverUrl: 'http://144.22.212.19:9001',
+    serverUrl: "http://144.22.212.19:9001",
     token: process.env.SONAR2_TOKEN,
     options: {
-      'sonar.projectKey': 'Track5Front',
-      'sonar.sources': '.',
-      'sonar.exclusions': '**/*.spec.ts,**/*.test.ts,**/node_modules/**',
-      'sonar.javascript.lcov.reportPaths': 'coverage/lcov.info',
-      'sonar.sourceEncoding': 'UTF-8',
+      "sonar.projectKey": "Track5Front",
+      "sonar.sources": ".",
+      "sonar.exclusions": "**/*.spec.ts,**/*.test.ts,**/node_modules/**",
+      "sonar.javascript.lcov.reportPaths": "coverage/lcov.info",
+      "sonar.sourceEncoding": "UTF-8",
     },
   },
-  () => process.exit()
+  () => process.exit(),
 );
